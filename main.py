3ed01b2bcb01a96012efd149d13844dd38101b8a
BHANU <jennyguddanti@gmail.com>
2023-06-30 18:07:09 +0200

    Test

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-
-rows = int(input("Enter the amount of rows you want to use"))
-columns = int(input("Enter the amount of columns you want to use"))
-symbols = str(input("Enter a symbol to use"))
-
-for i in range(rows):
-    for j in range(columns):
-        print(symbols,end="")
+print("ok bozo")
